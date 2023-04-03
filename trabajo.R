815ceb9c60b5c22b5244dc0ed48fdd84de672763
Ray Marcelo Ibarra <111158741+masteronprime@users.noreply.github.com>
2023-04-03 16:57:30 -0500

    codigos para aprender lenguaje R

---
+++ trabajo.R
@@ -0,0 +1,45 @@
+# Incluimos el paquete
+library(ggplot2)
+library(dplyr)
+library(ISLR)
+data(Carseats)
+str(Carseats)
+
+
+# Estudiemos el set de datos
+str(diamonds)
+head(diamonds)
+data(iris)
+str(iris)
+
+#listado para  
+ggplot(data = iris, aes(x = Sepal.Length,y = Sepal.Width)) +
+ geom_point(col="purple",size=3,form=diamonds)  + geom_line(aes(color=Species,linetype= Species))
+ 
+
+# Color como atributo
+ggplot(diamonds, aes(x = carat, y = price)) + 
+  geom_point(col="red1")
+
+# Relleno como aesthetic
+ggplot(diamonds, aes(x = clarity, fill=cut)) + 
+  geom_bar() 
+  #coord_flip()
+
+# Relleno como atributo
+ggplot(diamonds, aes(x = clarity)) + 
+  geom_bar(fill="skyblue4")
+
+#histograma
+ggplot(data=Carseats,aes(x=Sales, fill = ShelveLoc)) +
+  geom_histogram(bins=40)
+#diagrama de densidad 
+ggplot(data=Carseats,aes(x=Sales, fill = Urban)) +
+  geom_density(col="green")
+
+
+
+
+
+
+
